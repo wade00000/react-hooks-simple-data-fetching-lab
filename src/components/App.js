71228f40e54d6de2861b3f060fe1cdecfdfea31f
WADE <MEANTOINE12@GMAIL.COM>
2025-04-21 23:26:34 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
+import { useEffect,useState } from "react";
+
 // create your App component here
+function App(){
+    const [isLoaded,setIsLoaded] = useState(false)
+    const [fetchInfo,setFetchInfo] = useState([])
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res=>res.json())
+        .then(data=>{
+             setFetchInfo(data.message)
+             setIsLoaded(true)
+        })
+    },[])
+
+    if(!isLoaded){return <p>Loading...</p>}
+
+        return(
+            <img src={fetchInfo} alt="A Random Dog"/>
+        )
+
+
+}
+
+export default App
